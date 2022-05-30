2f8108ef4901ccd77bcdcd570d88c50eb2fa34d0
Александр <shamin_ad@inbox.ru>
2022-05-30 13:11:36 +0300

    first commit

---
+++ helpers/decorators.py
@@ -0,0 +1,44 @@
+import jwt
+from flask import request, abort
+
+from helpers.constants import JWT_SECRET, JWT_ALGORITHM
+
+
+def auth_required(func):
+    def wrapper(*args, **kwargs):
+        if 'AUTHORIZATION' not in request.headers:
+            abort(401)
+
+        data = request.headers['AUTHORIZATION']
+        token = data.split('Bearer ')[-1]
+
+        try:
+            jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        except Exception as e:
+            print('JWT Decode Exception', e)
+            abort(401)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def admin_required(func):
+    def wrapper(*args, **kwargs):
+        if 'AUTHORIZATION' not in request.headers:
+            abort(401)
+
+        data = request.headers['AUTHORIZATION']
+        token = data.split('Bearer ')[-1]
+        role = None
+
+        try:
+            user = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+            role = user.get('role', 'user')
+        except Exception as e:
+            print('JWT Decode Exception', e)
+            abort(401)
+
+        if role != 'admin':
+            abort(403)
+
+        return func(*args, **kwargs)
+    return wrapper
